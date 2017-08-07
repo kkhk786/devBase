93d9320a7612be8b01c8e04fa1aa53ba98c068f4
kkhk786 <kkhk786@gmail.com>
2017-08-07 10:19:47 +0900

    no memo
    no memo

--- devWorks_javaFundamental/TWS_Homeworks/Instruction008-1_thread/src/starterClass/StarterClass.java
+++ devWorks_javaFundamental/TWS_Homeworks/Instruction008-1_thread/src/starterClass/StarterClass.java
@@ -48,28 +48,27 @@ public class StarterClass {
 		/*
 		 * Horse game test
 		*/
-		
-		Thread t1 = new Thread(new Horse(1));
-		Thread t2 = new Thread(new Horse(2));
-		Thread t3 = new Thread(new Horse(3));
-		Thread t4 = new Thread(new Horse(4));
-		Thread t5 = new Thread(new Horse(5));
-		Thread t6 = new Thread(new Horse(6));
-		Thread t7 = new Thread(new Horse(7));
-		Thread t8 = new Thread(new Horse(8));
-		Thread t9 = new Thread(new Horse(9));
-		
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
-		t5.start();
-		t6.start();
-		t7.start();
-		t8.start();
-		t9.start();
-		
-		System.out.println("Terminates main() method...");
+//		Thread t1 = new Thread(new Horse(1));
+//		Thread t2 = new Thread(new Horse(2));
+//		Thread t3 = new Thread(new Horse(3));
+//		Thread t4 = new Thread(new Horse(4));
+//		Thread t5 = new Thread(new Horse(5));
+//		Thread t6 = new Thread(new Horse(6));
+//		Thread t7 = new Thread(new Horse(7));
+//		Thread t8 = new Thread(new Horse(8));
+//		Thread t9 = new Thread(new Horse(9));
+//		
+//		t1.start();
+//		t2.start();
+//		t3.start();
+//		t4.start();
+//		t5.start();
+//		t6.start();
+//		t7.start();
+//		t8.start();
+//		t9.start();
+//		
+//		System.out.println("Terminates main() method...");
 
 	}
 
